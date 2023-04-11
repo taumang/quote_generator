5c05869b9f1ca5e7a720b3f4405465b82b0db43f
Your Name <09taumang@gmail.com>
2023-04-11 10:23:12 +0200

    added a backend, for the API

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const quote_ = [
+
+]
+
+app.listen(3500,()=>{
+    console.log('Server is live on port 3500');
+})
